b8c593868395517d4434e322ca1af32afc061569
Ayodeji Oni <ayodejioni1505@gmail.com>
2023-12-29 00:30:59 +0100

    made changes to enhance interactivity

--- Week1/scripts/index-script.js
+++ Week1/scripts/index-script.js
@@ -1,3 +1,25 @@
-// let message = prompt("What is your name: ");
+const linkAnimations = document.querySelectorAll(".anchorLink");
 
-// alert("Merry christmas " + message)
+linkAnimations.forEach(function (linkAnimation) {
+  linkAnimation.addEventListener("mouseover", function () {
+    linkAnimation.style.color = "#fdfdfd";
+    linkAnimation.style.backgroundColor = "#010101";
+  });
+
+  linkAnimation.addEventListener("mouseout", function () {
+    linkAnimation.style.color = "#010101";
+    linkAnimation.style.backgroundColor = "#ffc300";
+  });
+});
+
+const resumeButton = document.getElementById("resume-button");
+
+resumeButton.addEventListener("mouseover", function () {
+    resumeButton.style.color = "#010101";
+    resumeButton.style.backgroundColor = "#ffc300";
+});
+
+resumeButton.addEventListener("mouseout", function () {
+    resumeButton.style.color = "#fdfdfd";
+    resumeButton.style.backgroundColor = "#010101";
+});
